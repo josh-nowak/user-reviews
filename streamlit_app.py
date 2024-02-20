e009a909505724bc2e788aa93a42d545d0b66394
carecodeconnect <carecodeconnect@pm.me>
2024-02-20 16:01:01 +0100

    added libraries and wordcloud call

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from src.utils import app_store_reviews
 from datetime import datetime
+import pandas as pd
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from PIL import Image
+import io
 
 st.header("App Store Review Analysis")
 
@@ -35,3 +40,32 @@ if st.button("Get reviews"):
     st.dataframe(reviews)
 
     st.markdown("*For now, this app only shows the reviews without any analysis.*")
+
+# Function to generate and display word cloud
+def generate_wordcloud(data):
+    # Combine all reviews into a single string
+    text = " ".join(review for review in data['review'].astype(str))
+    
+    # Create and generate a word cloud image with transparent background
+    wordcloud = WordCloud(background_color=None, mode="RGBA").generate(text)
+    
+    # Use plt to create a figure
+    plt.figure(figsize=(10, 6))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    
+    # Save plt figure to a bytes buffer
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png', transparent=True)
+    buf.seek(0)
+    
+    # Use PIL to open the bytes buffer as an Image, and then display it in Streamlit
+    image = Image.open(buf)
+    st.image(image, caption='Word Cloud', use_column_width=True)
+
+# Call the function to generate and display the word cloud
+# only call function if reviews have been retrieved
+if 'reviews' in locals():
+    st.title('Most Common Words in Reviews')
+    generate_wordcloud(reviews)
+
