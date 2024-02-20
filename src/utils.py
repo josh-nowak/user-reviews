d7aa035d2f64a20225e1212aac8957531ef8dd3c
carecodeconnect <carecodeconnect@pm.me>
2024-02-20 15:59:01 +0100

    added wordcloud function

--- src/utils.py
+++ src/utils.py
@@ -31,3 +31,25 @@ def app_store_reviews(url: str, n_last_reviews: int, after: str = False):
     reviews = reviews.loc[:, ["date", "title", "review", "rating"]]
     reviews = reviews.sort_values(by="date", ascending=False)
     return reviews
+
+# Function to generate and display word cloud
+def generate_wordcloud(data):
+    # Combine all reviews into a single string
+    text = " ".join(review for review in data['review'].astype(str))
+    
+    # Create and generate a word cloud image with transparent background
+    wordcloud = WordCloud(background_color=None, mode="RGBA").generate(text)
+    
+    # Use plt to create a figure
+    plt.figure(figsize=(10, 6))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    
+    # Save plt figure to a bytes buffer
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png', transparent=True)
+    buf.seek(0)
+    
+    # Use PIL to open the bytes buffer as an Image, and then display it in Streamlit
+    image = Image.open(buf)
+    st.image(image, caption='Word Cloud', use_column_width=True)
