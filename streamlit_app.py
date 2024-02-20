e6f1d270340c13bb2c01745295266706bcdb5714
josh-nowak <joshuanowak@icloud.com>
2024-02-20 13:01:03 +0100

    updated date handling again

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,23 +13,25 @@ st.markdown(
     "*For a demo, try this URL: https://apps.apple.com/de/app/slack/id618783545*"
 )
 
-n_last_reviews = st.number_input("Number of most recent reviews to analyze", value=1000)
+n_last_reviews = st.number_input("Number of most recent reviews to analyze", value=100)
+
 after_date = st.date_input(
     "Only reviews after this date",
     format="DD.MM.YYYY",
-    value=datetime.strptime("2024-01-01", "%Y-%m-%d"),
+    value=datetime.strptime("2023-01-01", "%Y-%m-%d"),
 )
-
 
 if st.button("Get reviews"):
 
-    st.markdown("*For now, this app only shows the reviews without any analysis.*")
-
     def get_reviews():
         return app_store_reviews(
-            url=app_store_url, n_last_reviews=n_last_reviews, after=after_date
+            url=app_store_url,
+            n_last_reviews=n_last_reviews,
+            after=after_date.strftime("%Y-%m-%d"),
         )
 
     reviews = get_reviews()
 
     st.dataframe(reviews)
+
+    st.markdown("*For now, this app only shows the reviews without any analysis.*")
