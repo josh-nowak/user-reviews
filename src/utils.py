879750820af55764c4bd60a13c9adc50c9084fe8
josh-nowak <joshuanowak@icloud.com>
2024-02-20 16:11:07 +0100

    improved date filter

--- src/utils.py
+++ src/utils.py
@@ -19,16 +19,38 @@ def app_data_from_url(url):
     return country, app_name, app_id
 
 
-def app_store_reviews(url: str, n_last_reviews: int, after: str = False):
+def app_store_reviews(
+    url: str, n_last_reviews: int = 1e6, start_date: str = None, end_date: str = None
+):
+
+    # Create AppStore object based on URL
     country, app_name, app_id = app_data_from_url(url)
     app = AppStore(country=country, app_name=app_name, app_id=app_id)
 
-    if after:
-        after = datetime.strptime(after, "%Y-%m-%d")
+    # Convert dates to datetime objects
+    if start_date:
+        start_date = datetime.strptime(start_date, "%Y-%m-%d")
+    else:
+        start_date = datetime.strptime("2000-01-01", "%Y-%m-%d")
 
-    app.review(how_many=n_last_reviews, after=after)
+    if end_date:
+        end_date = datetime.strptime(end_date, "%Y-%m-%d")
+    else:
+        end_date = datetime.now()
+
+    # Scrape reviews for the specified App
+    app.review(how_many=n_last_reviews, after=start_date)
+
+    # Convert response to dataframe
     reviews = pd.DataFrame(app.reviews)
+
+    # Keep only relevant columns
     reviews = reviews.loc[:, ["date", "title", "review", "rating"]]
+
+    # Filter using end_date (start_date is implemented in app_store_scraper already)
+    reviews = reviews[reviews["date"] < end_date]
+
+    # Sort by date
     reviews = reviews.sort_values(by="date", ascending=False)
     return reviews
 
--- streamlit_app.py
+++ streamlit_app.py
@@ -18,54 +18,65 @@ st.markdown(
     "*For a demo, try this URL: https://apps.apple.com/de/app/slack/id618783545*"
 )
 
-n_last_reviews = st.number_input("Number of most recent reviews to analyze", value=100)
+today = datetime.datetime.now()
+jan_1 = datetime.date(today.year, 1, 1)
 
-after_date = st.date_input(
-    "Only reviews after this date",
+
+date_range = st.date_input(
+    "Date range",
+    value=(jan_1, today),
+    max_value=today,
     format="DD.MM.YYYY",
-    value=datetime.strptime("2023-01-01", "%Y-%m-%d"),
 )
+start_date = date_range[0].strftime("%Y-%m-%d")
+
+if len(date_range) > 1:
+    end_date = date_range[1].strftime("%Y-%m-%d")
+
+
+def get_reviews():
+    reviews = app_store_reviews(
+        url=app_store_url, start_date=start_date, end_date=end_date
+    )
+    return reviews
+
 
 if st.button("Get reviews"):
 
-    def get_reviews():
-        return app_store_reviews(
-            url=app_store_url,
-            n_last_reviews=n_last_reviews,
-            after=after_date.strftime("%Y-%m-%d"),
-        )
+    if start_date and end_date:
+        if start_date < end_date:
+            reviews = get_reviews()
+            st.dataframe(reviews)
+            st.markdown(
+                "*For now, this app only shows the reviews without any analysis.*"
+            )
 
-    reviews = get_reviews()
-
-    st.dataframe(reviews)
-
-    st.markdown("*For now, this app only shows the reviews without any analysis.*")
 
 # Function to generate and display word cloud
 def generate_wordcloud(data):
     # Combine all reviews into a single string
-    text = " ".join(review for review in data['review'].astype(str))
-    
+    text = " ".join(review for review in data["review"].astype(str))
+
     # Create and generate a word cloud image with transparent background
     wordcloud = WordCloud(background_color=None, mode="RGBA").generate(text)
-    
+
     # Use plt to create a figure
     plt.figure(figsize=(10, 6))
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis('off')
-    
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+
     # Save plt figure to a bytes buffer
     buf = io.BytesIO()
-    plt.savefig(buf, format='png', transparent=True)
+    plt.savefig(buf, format="png", transparent=True)
     buf.seek(0)
-    
+
     # Use PIL to open the bytes buffer as an Image, and then display it in Streamlit
     image = Image.open(buf)
-    st.image(image, caption='Word Cloud', use_column_width=True)
+    st.image(image, caption="Word Cloud", use_column_width=True)
+
 
 # Call the function to generate and display the word cloud
 # only call function if reviews have been retrieved
-if 'reviews' in locals():
-    st.title('Most Common Words in Reviews')
+if "reviews" in locals():
+    st.title("Most Common Words in Reviews")
     generate_wordcloud(reviews)
-
