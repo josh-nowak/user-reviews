cd672e9f0188dbda9811f3a04acf019684244187
josh-nowak <joshuanowak@icloud.com>
2024-02-20 16:27:29 +0100

    refactoring with utils.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,9 @@
 import streamlit as st
-from src.utils import app_store_reviews
-from datetime import datetime
+from src.utils import app_store_reviews, generate_wordcloud
+import datetime
 import pandas as pd
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-from PIL import Image
-import io
 
-st.header("App Store Review Analysis")
+st.title("App Store Review Analysis")
 
 app_store_url = st.text_input(
     "**Your App Store URL** 📱",
@@ -51,32 +47,7 @@ if st.button("Get reviews"):
                 "*For now, this app only shows the reviews without any analysis.*"
             )
 
+    st.header("Most Common Words in Reviews")
 
-# Function to generate and display word cloud
-def generate_wordcloud(data):
-    # Combine all reviews into a single string
-    text = " ".join(review for review in data["review"].astype(str))
-
-    # Create and generate a word cloud image with transparent background
-    wordcloud = WordCloud(background_color=None, mode="RGBA").generate(text)
-
-    # Use plt to create a figure
-    plt.figure(figsize=(10, 6))
-    plt.imshow(wordcloud, interpolation="bilinear")
-    plt.axis("off")
-
-    # Save plt figure to a bytes buffer
-    buf = io.BytesIO()
-    plt.savefig(buf, format="png", transparent=True)
-    buf.seek(0)
-
-    # Use PIL to open the bytes buffer as an Image, and then display it in Streamlit
-    image = Image.open(buf)
+    image = generate_wordcloud(reviews)
     st.image(image, caption="Word Cloud", use_column_width=True)
-
-
-# Call the function to generate and display the word cloud
-# only call function if reviews have been retrieved
-if "reviews" in locals():
-    st.title("Most Common Words in Reviews")
-    generate_wordcloud(reviews)
